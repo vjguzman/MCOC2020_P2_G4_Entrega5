aa0ad6630b4e1bfd52f4c31168616d541b01aff4
Veronica Guzman <48454058+vjguzman@users.noreply.github.com>
2020-10-20 19:09:58 -0300

    archivos

---
+++ caso_D.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 20 16:44:57 2020
+
+@author: TOSHIBA 2IN1
+"""
+
---
+++ caso_L.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 20 17:01:18 2020
+
+@author: TOSHIBA 2IN1
+"""
+
